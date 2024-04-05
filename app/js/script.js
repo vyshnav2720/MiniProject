8b272e406d754060972d364bc21afb4c10299a4a
vyshnav2720 <vyshnav2720@outlook.com>
2024-04-06 00:43:10 +0530

    .

--- app/js/script.js
+++ app/js/script.js
@@ -1,33 +1,46 @@
-//Snap to section upon "EXPLORE"
-document.addEventListener('DOMContentLoaded', function() {
-    const exploreButton = document.querySelector('.hero__explore-button');
-    const page1Section = document.querySelector('.page1');
 
-    exploreButton.addEventListener('click', function() {
-        page1Section.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-    });
+const mastersection = document.querySelectorAll('.master-container');
+const sections = document.querySelectorAll('.master-container section');
+const hsnap1 = document.querySelectorAll('.master-container .hsnap1');
+const vsnap1 = document.querySelectorAll('.master-container .vsnap1');
+const vsnap2 = document.querySelectorAll('.master-container .vsnap2');
+const vsnap3 = document.querySelectorAll('.master-container .vsnap3');
+const hsnap2 = document.querySelectorAll('.master-container .hsnap2');
+
+const options = {
+  threshold: 0.5 // Defines at what percentage of the target's visibility the observer's callback should be executed
+};
+
+
+function Snapper(className){
+    if(className == "vsnap1"){
+    }
+    
+}
+
+
+const callback = (entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('active-section');
+      Snapper(entry.target.classList[0]);
+
+      
+      sections.forEach(section => {
+        if (section !== entry.target) {
+          section.classList.remove('active-section');
+        }
+      });
+    }
+  });
+};
+
+const observer = new IntersectionObserver(callback, options);
+
+sections.forEach(section => {
+  observer.observe(section);
 });
 
-
-
-
-
-//Circular Text
-const hero_circle_text = document.querySelector(".hero .hero__circle-text p")
-hero_circle_text.innerHTML = hero_circle_text.innerText.split("").map(
-    (char, i) => `<span style="transform:rotate(${i * 13}deg)">${char}</span>`
-).join("")
-
-// -----------
-
-
-
-//Scroll Snap
-const container = document.querySelector('.master-containe');
-const items = document.querySelectorAll('.pages');
 
 
 
